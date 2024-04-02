e97e5bf19e32d14675799b1fa8e44d2feb5c5d64
MelodyMindOfficial <mm.business@internet.ru>
2024-04-02 21:25:08 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/melodymindofficial.github.io",
+  // base: "/melodymindofficial.github.io",
   plugins: [react()],
 })
