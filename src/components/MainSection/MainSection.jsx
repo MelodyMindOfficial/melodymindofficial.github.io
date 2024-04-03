47050025b8e9fe0e89fb9d45655ade4e0c5c1b25
MelodyMindOfficial <mm.business@internet.ru>
2024-04-03 23:05:45 +0300

    fix

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,9 +1,21 @@
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
 import { mainTrends } from '../../data';
-import './MainSection.css';
 import noneImage from '/none_image.png';
+import './MainSection.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function MainSection({ children }) {
+    var settings = {
+        arrows: false,
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 6,
+        swipeToSlide: true,
+    };
+
     return (
         <section className="mainSection">
             <div className="_container">
@@ -16,43 +28,47 @@ export default function MainSection({ children }) {
                         <i className="fa-solid fa-chevron-right"></i>
                     </h4>
                 </div>
-                <div className="sectionTracks">
+                <Slider {...settings}>
                     {children.map((item) => (
-                        <div className="trackSection" key={item.image}>
-                            <div>
-                                <img
-                                    src={item.image ? item.image : noneImage}
-                                    alt="Track"
-                                />
+                        <div key={item.image}>
+                            <div className="trackSection">
+                                <div>
+                                    <img
+                                        src={
+                                            item.image ? item.image : noneImage
+                                        }
+                                        alt="Track"
+                                    />
+                                </div>
+                                <p
+                                    style={{
+                                        cursor: 'default',
+                                        userSelect: 'none',
+                                    }}
+                                    to={'/' + item.id}
+                                    id="price"
+                                    className="trackItem"
+                                >
+                                    {item.price && item.price + ' рублей'}
+                                </p>
+                                <Link
+                                    to={'/' + item.id}
+                                    id="title"
+                                    className="trackItem"
+                                >
+                                    {item.title}
+                                </Link>
+                                <Link
+                                    to={'/' + item.idAuthor}
+                                    id="author"
+                                    className="trackItem"
+                                >
+                                    {item.author}
+                                </Link>
                             </div>
-                            <p
-                                style={{
-                                    cursor: 'default',
-                                    userSelect: 'none',
-                                }}
-                                to={'/' + item.id}
-                                id="price"
-                                className="trackItem"
-                            >
-                                {item.price && item.price + ' рублей'}
-                            </p>
-                            <Link
-                                to={'/' + item.id}
-                                id="title"
-                                className="trackItem"
-                            >
-                                {item.title}
-                            </Link>
-                            <Link
-                                to={'/' + item.idAuthor}
-                                id="author"
-                                className="trackItem"
-                            >
-                                {item.author}
-                            </Link>
                         </div>
                     ))}
-                </div>
+                </Slider>
             </div>
         </section>
     );
