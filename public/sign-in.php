beaeb0fb5491c3cdd83a90ee6113ee93eefc7a80
MelodyMindOfficial <mm.business@internet.ru>
2024-04-13 13:22:02 +0300

    login

--- public/sign-in.php
+++ public/sign-in.php
@@ -1,18 +1,18 @@
 <?php
-session_start();
+// session_start();
 require_once('connect.php');
 
 $email = $_POST['email'];
 $password = md5($_POST['password']);
 
-$sql = "SELECT * FROM `users` WHERE email = '$email' AND password = '$password'";
-$result = $connect->query($sql);
-if ($result->num_rows > 0) {
-    $user = mysqli_fetch_assoc(mysqli_query($link, $sql));
-    $_SESSION['user'] = [$user['id'], $user['name'], $user['surname'], $user['email'], $user['password'], $user['status']];
-    $_SESSION['message'] = "Вы успешно вошли";
+$sql = mysqli_query($link, "SELECT * FROM users WHERE email = '$email' && password = '$password'");
+
+if (mysqli_num_rows($sql) > 0) {
+    // $user = mysqli_fetch_assoc(mysqli_query($link, $sql));
+    // $_SESSION['user'] = [$user['id'], $user['name'], $user['surname'], $user['email'], $user['password'], $user['status']];
+    // $_SESSION['message'] = "Вы успешно вошли";
     header('Location: https://cg30388.tw1.ru/profile');
 } else {
-    $_SESSION['message'] = "Такого пользователя не существует";
+    // $_SESSION['message'] = "Такого пользователя не существует";
     header('Location: https://cg30388.tw1.ru/feed');
 }
