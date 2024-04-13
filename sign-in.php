7bc0159fefded5578d1dc57a92a6e78833a68e21
MelodyMindOfficial <mm.business@internet.ru>
2024-04-13 14:14:23 +0300

    Updates

--- sign-in.php
+++ sign-in.php
@@ -8,11 +8,11 @@ $password = md5($_POST['password']);
 $sql = mysqli_query($link, "SELECT * FROM users WHERE email = '$email' && password = '$password'");
 
 if (mysqli_num_rows($sql) > 0) {
-    // $user = mysqli_fetch_assoc(mysqli_query($link, $sql));
-    // $_SESSION['user'] = [$user['id'], $user['name'], $user['surname'], $user['email'], $user['password'], $user['status']];
-    // $_SESSION['message'] = "Вы успешно вошли";
+    $user = mysqli_fetch_assoc($sql);
+    $_SESSION['user'] = [$user['id'], $user['name'], $user['email'], $user['password']];
+    $_SESSION['message'] = "Вы успешно вошли";
     header('Location: https://cg30388.tw1.ru/profile');
 } else {
-    // $_SESSION['message'] = "Такого пользователя не существует";
+    $_SESSION['message'] = "Такого пользователя не существует";
     header('Location: https://cg30388.tw1.ru/feed');
 }
