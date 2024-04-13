52827643f2c038497a27d0e1f958c01bd2a072f6
MelodyMindOfficial <mm.business@internet.ru>
2024-04-13 12:40:39 +0300

    Updates

--- sign-in.php
+++ sign-in.php
@@ -1,28 +1,18 @@
 <?php
-
+session_start();
 require_once('connect.php');
 
 $email = $_POST['email'];
-$password = $_POST['password'];
-$id = rand(100000, 999999);
+$password = md5($_POST['password']);
 
-$id_sql = mysqli_query($link, "SELECT * FROM users WHERE id = '$id'");
-while (mysqli_fetch_assoc($id_sql)) {
-    $id = rand(100000, 999999);
-}
-
-$sql = mysqli_query($link, "SELECT * FROM users WHERE email = '$email'");
-if (!mysqli_fetch_assoc($sql)) {
-
-    $password = md5($password);
-    $sql = "INSERT INTO users (id, email, password) VALUES ('$id', '$email', '$password')";
-
-    if ($connect->query($sql)) {
-        header('Location: https://cg30388.tw1.ru/profile');
-    } else {
-        header('Location: https://cg30388.tw1.ru/');
-    }
-    $link->close();
+$sql = "SELECT * FROM `users` WHERE email = '$email' AND pass = '$password'";
+$result = $connect->query($sql);
+if ($result->num_rows > 0) {
+    $user = mysqli_fetch_assoc(mysqli_query($link, $sql));
+    $_SESSION['user'] = [$user['id'], $user['name'], $user['surname'], $user['email'], $user['password'], $user['status']];
+    $_SESSION['message'] = "Вы успешно вошли";
+    header('Location: https://cg30388.tw1.ru/profile');
 } else {
+    $_SESSION['message'] = "Такого пользователя не существует";
     header('Location: https://cg30388.tw1.ru/');
 }
