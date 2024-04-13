1c42764bd150ad508ed01e5149cbf73ad44b71db
MelodyMindOfficial <mm.business@internet.ru>
2024-04-13 16:55:57 +0300

    login

--- public/sign-up.php
+++ public/sign-up.php
@@ -1,63 +1,33 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST");
-header("Access-Control-Allow-Headers: Content-Type");
+require_once('connect.php');
 
-$conn = new mysqli("localhost", "root", "root", "users");
+$eData = file_get_contents("php://input");
+$dData = json_decode($eData, true);
 
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit();
-} else {
-    $eData = file_get_contents("php://input");
-    $dData = json_decode($eData, true);
+$email = $dData['email'];
+$password = $dData['password'];
+$id = rand(100000, 999999);
 
-    $email = $dData['email'];
-    $password = $dData['password'];
-
-    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
-    $res = mysqli_query($conn, $sql);
-
-    $result = "";
-
-    if ($res) {
-        $result = "Вы успешно зарегестрировались";
-    } else {
-        $result = "";
-    }
+$id_sql = mysqli_query($link, "SELECT * FROM users WHERE id = '$id'");
+while (mysqli_fetch_assoc($id_sql)) {
+    $id = rand(100000, 999999);
 }
 
-$conn->close();
+$sql = mysqli_query($link, "SELECT * FROM users WHERE email = '$email'");
+if (!mysqli_fetch_assoc($sql)) {
+    $password = md5($password);
+    $sql = "INSERT INTO users (id, email, password) VALUES ('$id', '$email', '$password')";
+
+    if ($connect->query($sql)) {
+        $result = "Вы успешно зарегестрировались";
+    } else {
+        $result = "Ошибка регистрации";
+    }
+} else {
+    $result = "Такой пользователь уже существует!";
+}
+
+$link->close();
 $response[] = array("result" => $result);
 echo json_encode($response);
-// session_start();
-// require_once('connect.php');
-
-// $email = $_POST['email'];
-// $password = $_POST['password'];
-// $id = rand(100000, 999999);
-
-// $id_sql = mysqli_query($link, "SELECT * FROM users WHERE id = '$id'");
-// while (mysqli_fetch_assoc($id_sql)) {
-//     $id = rand(100000, 999999);
-// }
-
-// $sql = mysqli_query($link, "SELECT * FROM users WHERE email = '$email'");
-// if (!mysqli_fetch_assoc($sql)) {
-
-//     $password = md5($password);
-//     $sql = "INSERT INTO users (id, email, password) VALUES ('$id', '$email', '$password')";
-
-//     if ($connect->query($sql)) {
-//         $_SESSION['message'] = "Вы успешно зарегестрировались";
-//         header('Location: https://cg30388.tw1.ru/profile');
-//     } else {
-//         $_SESSION['message'] = "Ошибка регистрации";
-//         header('Location: https://cg30388.tw1.ru/');
-//     }
-//     $link->close();
-// } else {
-//     $_SESSION['message'] = "Такой пользователь существует";
-//     header('Location: https://cg30388.tw1.ru/feed');
-// }
