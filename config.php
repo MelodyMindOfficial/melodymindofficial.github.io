736bbcb8fa93ca68a2ac1db3c4bbaffe97ea4d18
MelodyMindOfficial <mm.business@internet.ru>
2024-04-17 17:52:24 +0300

    Updates

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-
-if ($_SESSION['user']) {
-    $response[] = $_SESSION['user'];
-    echo json_encode($response);
-}
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['user']);
-$result = "Вы вышли из аккаунта";
-$response[] = array("result" => $result, "login" => false);
-echo json_encode($response);
