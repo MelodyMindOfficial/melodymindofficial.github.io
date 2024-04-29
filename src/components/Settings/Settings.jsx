76315c40064bb3d6f912365044a5090c0a17c38c
MelodyMindOfficial <mm.business@internet.ru>
2024-04-29 18:55:57 +0300

    fix

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -125,10 +125,89 @@ export default function Settings({ authorized, isMsg }) {
                     </aside>
                     <div className="settingsSectionMain">
                         {sectionProfile == 'profile' && (
-                            <SettingsProfile
-                                updateProfile={updateProfile}
-                                auth={auth}
-                            />
+                            <form method="" onSubmit={updateProfile}>
+                                <section className="settingsImage">
+                                    <img src="./images/user.png" alt="" />
+                                    <button>
+                                        {language == 'en'
+                                            ? 'Upload image'
+                                            : 'Загрузить изображение'}
+                                        <i className="fa-solid fa-chevron-down"></i>
+                                    </button>
+                                </section>
+                                <section className="settingsText">
+                                    <label htmlFor="name">
+                                        {language == 'en' ? 'Name' : 'Имя'}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        pattern="([A-Za-zА-Яа-яЁё]+[\-\s]?){3,}"
+                                        value={isname}
+                                        onChange={(event) =>
+                                            setName(event.target.value)
+                                        }
+                                    />
+                                    <label htmlFor="surname">
+                                        {language == 'en'
+                                            ? 'Last name'
+                                            : 'Фамилия'}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="surname"
+                                        pattern="([A-Za-zА-Яа-яЁё]+[\-\s]?){3,}"
+                                        value={issurname}
+                                        onChange={(event) =>
+                                            setSurname(event.target.value)
+                                        }
+                                    />
+                                    <label htmlFor="nickname">
+                                        {language == 'en'
+                                            ? ''
+                                            : 'Отображаемое имя'}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="nickname"
+                                        value={isdisplayName}
+                                        onChange={(event) =>
+                                            setDisplayName(event.target.value)
+                                        }
+                                    />
+                                    <label htmlFor="location">
+                                        {language == 'en'
+                                            ? ''
+                                            : 'Местоположение'}
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="location"
+                                        value={islocation}
+                                        onChange={(event) =>
+                                            setLocation(event.target.value)
+                                        }
+                                    />
+                                    <label htmlFor="bio">
+                                        {language == 'en' ? '' : 'Биография'}
+                                    </label>
+                                    <textarea
+                                        name="bio"
+                                        id=""
+                                        cols="30"
+                                        rows="10"
+                                        value={isbio}
+                                        onChange={(event) => {
+                                            setBio(event.target.value);
+                                        }}
+                                    ></textarea>
+                                    <button type="submit">
+                                        {language == 'en'
+                                            ? 'Save changes'
+                                            : 'Сохранить изменения'}
+                                    </button>
+                                </section>
+                            </form>
                         )}
                         {sectionProfile == 'credit' && <SettingsCredit />}
                         {sectionProfile == 'credit' && <SettingsSocial />}
