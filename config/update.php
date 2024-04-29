e4f09046babcc802e973911bce017edf23d3e24d
MelodyMindOfficial <mm.business@internet.ru>
2024-04-29 18:56:07 +0300

    Updates

--- config/update.php
+++ config/update.php
@@ -23,7 +23,7 @@ if (!mysqli_fetch_assoc($sql)) {
         $result = "Такой пользователь не существует!";
     }
 } else {
-    $sql = "UPDATE `users` SET `name` = '$name', `surname` = '$surname', `displayName` = '$displayName', `location` = '$location', `bio` = \"$bio\" WHERE `users`.`email` = '$email'";
+    $sql = "UPDATE `users` SET `name` = '$name', `surname` = '$surname', `displayName` = '$displayName', `location` = '$location', `bio` = \"$bio\" WHERE `users`.`id` = '$id'";
 
     if ($connect->query($sql)) {
         if ($language == 'en') {
