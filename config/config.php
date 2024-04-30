d937d0e20b7db1ece25aa15336fb6afced8043e2
MelodyMindOfficial <mm.business@internet.ru>
2024-04-30 15:00:18 +0300

    Updates

--- config/config.php
+++ config/config.php
@@ -3,6 +3,7 @@
 session_start();
 
 if ($_SESSION['user']) {
-    $response[] = $_SESSION['user'];
+    $_SESSION['userData'] = [true, $user['id'], $user['name'], $user['email'], $user['password'], $user['surname'], $user['displayName'], $user['photo'], $user['location'], $user['bio'], $user['status'], $user['subscription'], $user['followers'], $user['plays'], $user['tracks'],];
+    $response[] = $_SESSION['userData'];
     echo json_encode($response);
 }
