59e359f252fbd7f7ebedb270cb02950be15ced8a
MelodyMindOfficial <mm.business@internet.ru>
2024-05-06 13:44:02 +0300

    fix

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -13,6 +13,31 @@ export default function Profile({ authorized, isActive }) {
         }, 500);
     }
 
+    // var authorized = [
+    //     true,
+    //     999999,
+    //     'John',
+    //     'john1989@gmail.com',
+    //     '123John!!!',
+    //     'Smith',
+    //     'John_Smith',
+    //     null,
+    //     'the UK',
+    //     'I was born in London and I wanna tell you that I can speak only russian language, so Ima switching it. Всем привет, меня зовут Джон и я родился в Англии. Однако я хорошо знаю русский за счет того, что моя бабушка русская, и была против того, чтобы я говорил по англиски',
+    //     0,
+    //     false,
+    //     0,
+    //     0,
+    //     0,
+    //     79782156190,
+    //     false,
+    //     'johanSoundCloud',
+    //     'johanYoutube',
+    //     'johanRutube',
+    //     '@johanTik-Tok',
+    //     'johanTwitch',
+    // ];
+
     const auth = {
         login: authorized[0],
         id: authorized[1],
@@ -29,6 +54,13 @@ export default function Profile({ authorized, isActive }) {
         followers: authorized[12],
         plays: authorized[13],
         tracks: authorized[14],
+        phoneNumber: authorized[15],
+        Auth2: authorized[16],
+        soundCloud: authorized[17],
+        youtube: authorized[18],
+        rutube: authorized[19],
+        tiktok: authorized[20],
+        twitch: authorized[21],
     };
 
     return (
@@ -40,6 +72,7 @@ export default function Profile({ authorized, isActive }) {
                         <h3>
                             {auth.displayName ? auth.displayName : auth.email}
                         </h3>
+                        {auth.location && <p>{auth.location}</p>}
                     </div>
                     <Link to="/settings" onClick={() => isActive('main')}>
                         <button>
@@ -73,6 +106,132 @@ export default function Profile({ authorized, isActive }) {
                             </li>
                         </ul>
                     </div>
+                    {auth.tracks ? (
+                        <>
+                            <hr style={{ height: '2px' }} />
+                            <div className="profileInfoStat">
+                                <h4>{language == 'en' ? 'TRACKS' : 'ТРЕКИ'}</h4>
+                                <ul>
+                                    <li>
+                                        <p>{auth.tracks}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </>
+                    ) : null}
+                    {auth.bio ? (
+                        <>
+                            <hr style={{ height: '2px' }} />
+                            <div className="profileInfoStat">
+                                <h4>
+                                    {language == 'en' ? 'ABOUT ME' : 'О СЕБЕ'}
+                                </h4>
+                                <ul>
+                                    <li>
+                                        <p>{auth.bio}</p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </>
+                    ) : null}
+                    {auth.soundCloud ||
+                    auth.youtube ||
+                    auth.rutube ||
+                    auth.tiktok ||
+                    auth.twitch ? (
+                        <>
+                            <hr style={{ height: '2px' }} />
+                            <div className="profileInfoStat" id="social">
+                                <h4>
+                                    {language == 'en'
+                                        ? 'FIND ME ON'
+                                        : 'НАЙТИ МЕНЯ НА'}
+                                </h4>
+                                <ul>
+                                    {auth.soundCloud && (
+                                        <li>
+                                            <a
+                                                href={
+                                                    'https://soundcloud.com/' +
+                                                    auth.soundCloud
+                                                }
+                                                target="_blank"
+                                            >
+                                                <p>
+                                                    <i className="fa-brands fa-soundcloud"></i>{' '}
+                                                    Sound Cloud
+                                                </p>
+                                            </a>
+                                        </li>
+                                    )}
+                                    {auth.youtube && (
+                                        <li>
+                                            <a
+                                                href={
+                                                    'https://youtube.com/' +
+                                                    auth.youtube
+                                                }
+                                                target="_blank"
+                                            >
+                                                <p>
+                                                    <i className="fa-brands fa-youtube"></i>{' '}
+                                                    Youtube
+                                                </p>
+                                            </a>
+                                        </li>
+                                    )}
+                                    {auth.rutube && (
+                                        <li>
+                                            <a
+                                                href={
+                                                    'https://rutube.ru/channel/' +
+                                                    auth.rutube
+                                                }
+                                                target="_blank"
+                                            >
+                                                <p>
+                                                    <i className="fa-brands fa-rust"></i>{' '}
+                                                    Rutube
+                                                </p>
+                                            </a>
+                                        </li>
+                                    )}
+                                    {auth.tiktok && (
+                                        <li>
+                                            <a
+                                                href={
+                                                    'https://tiktok.com/' +
+                                                    auth.tiktok
+                                                }
+                                                target="_blank"
+                                            >
+                                                <p>
+                                                    <i className="fa-brands fa-tiktok"></i>{' '}
+                                                    Tik-Tok
+                                                </p>
+                                            </a>
+                                        </li>
+                                    )}
+                                    {auth.twitch && (
+                                        <li>
+                                            <a
+                                                href={
+                                                    'https://twitch.tv/' +
+                                                    auth.twitch
+                                                }
+                                                target="_blank"
+                                            >
+                                                <p>
+                                                    <i className="fa-brands fa-twitch"></i>{' '}
+                                                    Twitch
+                                                </p>
+                                            </a>
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
+                        </>
+                    ) : null}
                 </section>
                 <section className="profileTracks">
                     {false ? (
